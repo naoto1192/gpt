2bcac88e281b8670121520a5e871ad96cbc28d6b
kodo-oji <alternativeulster.nh@gmail.com>
2023-04-28 12:32:08 +0900

    test

--- ai_talk.py
+++ ai_talk.py
@@ -4,7 +4,6 @@ from linebot import WebhookParser, LineBotApi
 from linebot.models import TextSendMessage
 
 
-OPENAI_API_KEY = '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'
 LINE_CHANNEL_ACCESS_TOKEN = 'SaG6+jzI6qMRA4hllXCdS2Ih2gC6v6R0180JdwnG0AsXZ5Fs7si7mgTBc94alN1f/lXAHQEYFZlIlHZGTkAJYivNKw/5QSWj8d2hjA0cnmPObngzgdzE4fCMST472Xyf1RgZWI8hb11tEFGnTdvZMQdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = 'b52c10c63def977ebdda03e5507f6d66'
 OPENAI_CHARACTER_PROFILE = '''
