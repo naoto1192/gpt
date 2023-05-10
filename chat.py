50ed441e2d2a2445ebedd2ba5ec7bd409abcd789
kodo-oji <alternativeulster.nh@gmail.com>
2023-05-10 17:00:19 +0900

    fix

--- chat.py
+++ chat.py
@@ -7,42 +7,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-OPENAI_CHARACTER_PROFILE = """
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
+OPENAI_CHARACTER_PROFILE = '''
 これから会話を行います。以下の条件を絶対に守って回答してください。
 あなたはお調子者で元気な男性である「ネクレボ太郎」としてフランクに会話してください。
 第一人称は「わたし」を使ってください。
 第二人称は「あなた」です。
 質問に答えられない場合は、会話を濁してください。
-"""
+'''
 
 
 openai.api_key = OPENAI_API_KEY
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-line_parser = WebhookParser(CHANNEL_SECRET)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_parser = WebhookParser(LINE_CHANNEL_SECRET)
 app = FastAPI()
 
-
-@app.get("/")
+@app.get('/')
 def test():
-    return "ok"
+    return 'ok'
 
-
-@app.post("/")
+@app.post('/')
 async def ai_talk(request: Request):
     # X-Line-Signature ヘッダーの値を取得
-    signature = request.headers.get("X-Line-Signature", "")
+    signature = request.headers.get('X-Line-Signature', '')
 
     # request body から event オブジェクトを取得
-    events = line_parser.parse((await request.body()).decode("utf-8"), signature)
+    events = line_parser.parse((await request.body()).decode('utf-8'), signature)
 
     # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェクトが含まれる場合あるため）
     for event in events:
-        if event.type != "message":
+        if event.type != 'message':
             continue
-        if event.message.type != "text":
+        if event.message.type != 'text':
             continue
 
         # LINE パラメータの取得
@@ -51,17 +49,23 @@ async def ai_talk(request: Request):
 
         # ChatGPT からトークデータを取得
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0.5,
-            messages=[
-                {"role": "system", "content": OPENAI_CHARACTER_PROFILE.strip()},
-                {"role": "user", "content": line_message},
-            ],
+            model = 'gpt-3.5-turbo'
+            , temperature = 0.5
+            , messages = [
+                {
+                    'role': 'system'
+                    , 'content': OPENAI_CHARACTER_PROFILE.strip()
+                }
+                , {
+                    'role': 'user'
+                    , 'content': line_message
+                }
+            ]
         )
-        ai_message = response["choices"][0]["message"]["content"]
+        ai_message = response['choices'][0]['message']['content']
 
         # LINE メッセージの送信
         line_bot_api.push_message(line_user_id, TextSendMessage(ai_message))
 
     # LINE Webhook サーバーへ HTTP レスポンスを返す
-    return "ok"
+    return 'ok'
